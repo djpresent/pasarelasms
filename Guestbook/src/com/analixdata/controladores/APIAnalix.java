f43d168d84ab17dddca06fecbd26e13a69f4246f
Alina <Alina@DESKTOP-9ORRAB5>
2016-02-10 14:18:29 -0500

    api

--- Guestbook/src/com/analixdata/controladores/APIAnalix.java
+++ Guestbook/src/com/analixdata/controladores/APIAnalix.java
@@ -1,11 +1,14 @@
 package com.analixdata.controladores;
 
 import java.io.*;
+import java.nio.charset.Charset;
 import java.sql.*;
 
 import javax.servlet.http.*;
 
-import com.analixdata.modelos.Usuario;
+import org.apache.commons.codec.binary.Base64;
+
+
 import com.google.appengine.api.utils.SystemProperty;
 
 public class APIAnalix extends HttpServlet {
@@ -14,47 +17,37 @@ public class APIAnalix extends HttpServlet {
 	  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		
 		resp.setContentType("text/html");
-	    String url = null;
-	    
 
-	    PrintWriter out = resp.getWriter();
-	
-	   
-	      try {
-	        String id = req.getParameter("idempresa");
-	        String u = req.getParameter("usuario");
-	        String pass = req.getParameter("pass");
+	        String u;
+	        String pass;
+	        String numero;
+	        String mensaje;
+      
 	        
-	  
-	        
-	        String encabezado =req.getHeader("Authorization");
-	        
-	        
-	      //  ATRIBUTO: "+atributo+" ENCABEZADO "+encabezado
-	        
-	       /* final String authorization = httpRequest.getHeader("Authorization");
+	      String authorization = req.getHeader("Authorization");
 	        if (authorization != null && authorization.startsWith("Basic")) {
 	            // Authorization: Basic base64credentials
 	            String base64Credentials = authorization.substring("Basic".length()).trim();
-	            String credentials = new String(Base64.getDecoder().decode(base64Credentials),
+	            String credentials = new String(Base64.decodeBase64(base64Credentials),
 	                    Charset.forName("UTF-8"));
 	            // credentials = username:password
-	            final String[] values = credentials.split(":",2);
-	*/
-	   
-		         //   out.println( "IDEMPRESA: "+id+" USUARIO:"+u+" PASS: "+pass+" ENCABEZADO "+encabezado);
-		       
-	        out.println( "RESPUESTA");
+	            String[] values = credentials.split(":",2);
+	            u=values[0];
+	            pass=values[2];
+	            
+	            numero=req.getParameter("numero");
+	            mensaje=req.getParameter("mensaje");
+
+	       		PrintWriter out = resp.getWriter();
+	       		if(numero!=null && mensaje!=null){
+	       			out.println("CORRECTO");
+		        }else{
+		        	out.println("PARAMETROS INCORRECTOS");
+		        }
+		   
+		        out.close();
 	      
-	    } catch (Error e) {
-	      e.printStackTrace();
-	      out.println("ERROR");
-	    
-		} finally {
-	  	  
-	    }
-	    //resp.sendRedirect("/empresa.jsp");
+	        }
 
-	  }
-
-}
+	}
+	}
